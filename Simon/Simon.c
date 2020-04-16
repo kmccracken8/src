bbde3d94bc767566e5aaa4d5fa7805acca7d4227
kmccracken8 <kmccracken@olin.edu>
2020-04-16 16:56:06 -0400

    introduced go and mode buttons

--- Simon/Simon.c
+++ Simon/Simon.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 #include <math.h>
 
-#define COIN                  SW1
-#define MODE_SW               SW1
-#define GO_SW                 SW2
+#define MODE_SW               D8
+#define GO_SW                 D9
 
-#define RED_LED               LED1
-//#define YELLOW_LED            D1
-#define BLUE_LED              LED3
-#define WHITE_LED             LED2
+#define LED_1                 D4
+#define LED_2                 D5
+#define LED_3                 D10
+#define LED_4                 D7
 
-#define RED_SW                SW3
-#define BLUE_SW               SW1
-//#define YELLOW_SW             D1
-#define WHITE_SW              SW2
+
+#define SW_1                  D0
+#define SW_2                  D1
+#define SW_3                  D2
+#define SW_4                  D3
 
 
 typedef void(*STATE_HANDLER_T)(void);
@@ -25,7 +25,7 @@ void gameplay(void);
 void scoredisplay(void);
 
 STATE_HANDLER_T state, last_state;
-uint16_t mode, mode_sel, GO_SW_pressed, COIN_on, MODE_SW_pressed, BLUE_SW_pressed, WHITE_SW_pressed, RED_SW_pressed, round_num;
+uint16_t mode, mode_sel, GO_SW_pressed, MODE_SW_pressed, SW_1_pressed, SW_2_pressed, SW_3_pressed, SW_4_pressed, round_num;
 uint16_t i, j, k, counter, tcount, game_state, lit, unlit, lose, SWval;
 uint16_t seed, game[255], input[255];
 
@@ -42,12 +42,12 @@ int16_t main(void){
     IFS0bits.T1IF = 0;      // lower Timer1 interrupt flag
     T1CONbits.TON = 1;      // Starts timer
 
-    BLUE_SW_pressed = 0;
-    WHITE_SW_pressed = 0;
-    RED_SW_pressed = 0;
+    SW_1_pressed = 0;
+    SW_2_pressed = 0;
+    SW_3_pressed = 0;
+    SW_4_pressed = 0;
     MODE_SW_pressed = 0;
     GO_SW_pressed = 0;
-    COIN_on = 0;
 
     mode_sel = 0;
     tcount = 0;
@@ -58,6 +58,19 @@ int16_t main(void){
 
     state = modeselect;
     last_state = (STATE_HANDLER_T)NULL;
+
+    D0_DIR = IN;
+    D1_DIR = IN;
+    D2_DIR = IN;
+    D3_DIR = IN;
+    D4_DIR = OUT;
+    D5_DIR = OUT;
+    D10_DIR = OUT;
+    D7_DIR = OUT;
+    D8_DIR = IN;
+    D9_DIR = IN;
+
+    resetLED();
 
     while(1){
         state();
@@ -89,7 +102,7 @@ void modeselect(void){
       if(MODE_SW == 0 && MODE_SW_pressed == 0){          //detect sw1
         MODE_SW_pressed = 1;
         resetLED();
-        if (mode_sel < 2) {                      //advance mode selected
+        if (mode_sel < 3) {                      //advance mode selected
           mode_sel = mode_sel + 1;
         }else{
           mode_sel = 0;
@@ -118,7 +131,7 @@ void gameplay(void){
         last_state = state;
 
         for (i = 0; i < 255; i++) {
-          game[i] = rand() % 3;
+          game[i] = rand() % 4;
         }
         game_state = 0;
         tcount = 0;
@@ -160,9 +173,10 @@ void gameplay(void){
               game_state = 2;
               j = 0;
               k = 0;
-              BLUE_SW_pressed = 0;
-              WHITE_SW_pressed = 0;
-              RED_SW_pressed = 0;
+              SW_1_pressed = 0;
+              SW_2_pressed = 0;
+              SW_3_pressed = 0;
+              SW_4_pressed = 0;
               tcount = 0;
           }
           break;
@@ -171,36 +185,8 @@ void gameplay(void){
           if(IFS0bits.T1IF == 1){
             IFS0bits.T1IF = 0;
 
-            if(BLUE_SW == 0 && BLUE_SW_pressed == 0){
-              BLUE_SW_pressed = 1;
-              if(game[k] != 2){                               //Lose Condition
-                lose = 1;
-              }else{
-                k = k + 1;
-                tcount = 0;
-              }
-              lightLED(2);
-            }else if(BLUE_SW == 1 && BLUE_SW_pressed == 1){
-              BLUE_SW_pressed = 0;
-              resetLED();
-            }
-
-            if(WHITE)SW == 0 && WHITE_SW_pressed == 0){
-              WHITE_SW_pressed = 1;
-              if(game[k] != 1){                               //Lose Condition
-                lose = 1;
-              }else{
-                k = k + 1;
-                tcount = 0;
-              }
-              lightLED(1);
-            }else if(WHITE_SW == 1 && WHITE_SW_pressed == 1){
-              WHITE_SW_pressed = 0;
-              resetLED();
-            }
-
-            if(RED_SW == 0 && RED_SW_pressed == 0){
-              RED_SW_pressed = 1;
+            if(SW_1 == 0 && SW_1_pressed == 0){
+              SW_1_pressed = 1;
               if(game[k] != 0){                               //Lose Condition
                 lose = 1;
               }else{
@@ -208,8 +194,50 @@ void gameplay(void){
                 tcount = 0;
               }
               lightLED(0);
-            }else if(RED_SW == 1 && RED_SW_pressed == 1){
-              RED_SW_pressed = 0;
+            }else if(SW_1 == 1 && SW_1_pressed == 1){
+              SW_1_pressed = 0;
+              resetLED();
+            }
+
+            if(SW_2 == 0 && SW_2_pressed == 0){
+              SW_2_pressed = 1;
+              if(game[k] != 1){                               //Lose Condition
+                lose = 1;
+              }else{
+                k = k + 1;
+                tcount = 0;
+              }
+              lightLED(1);
+            }else if(SW_2 == 1 && SW_2_pressed == 1){
+              SW_2_pressed = 0;
+              resetLED();
+            }
+
+            if(SW_3 == 0 && SW_3_pressed == 0){
+              SW_3_pressed = 1;
+              if(game[k] != 2){                               //Lose Condition
+                lose = 1;
+              }else{
+                k = k + 1;
+                tcount = 0;
+              }
+              lightLED(2);
+            }else if(SW_3 == 1 && SW_3_pressed == 1){
+              SW_3_pressed = 0;
+              resetLED();
+            }
+
+            if(SW_4 == 0 && SW_4_pressed == 0){
+              SW_4_pressed = 1;
+              if(game[k] != 3){                               //Lose Condition
+                lose = 1;
+              }else{
+                k = k + 1;
+                tcount = 0;
+              }
+              lightLED(3);
+            }else if(SW_4 == 1 && SW_4_pressed == 1){
+              SW_4_pressed = 0;
               resetLED();
             }
 
@@ -246,6 +274,7 @@ void scoredisplay(void){
         lightLED(0);
         lightLED(1);
         lightLED(2);
+        lightLED(3);
     }
 
     if(state != last_state){
@@ -254,21 +283,25 @@ void scoredisplay(void){
 }
 
 int resetLED(){
-    BLUE_LED = 0;
-    RED_LED = 0;
-    WHITE_LED = 0;
+    LED_1 = 0;
+    LED_2 = 0;
+    LED_3 = 0;
+    LED_4 = 0;
 }
 
 int lightLED(int id){
     switch (id) {
       case 0:
-        LED1 = 1;
+        LED_1 = 1;
         break;
       case 1:
-        LED2 = 1;
+        LED_2 = 1;
         break;
       case 2:
-        LED3 = 1;
+        LED_3 = 1;
+        break;
+      case 3:
+        LED_4 = 1;
         break;
     }
 }
